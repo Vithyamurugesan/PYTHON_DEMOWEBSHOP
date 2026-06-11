b15e44b1128c4486a1adad940b4792d89e438bc3
Vithyamurugesan <vithyabtech54@gmail.com>
2026-06-12 00:03:51 +0530

    Updated Register Action

--- Actions/RegisterAction.py
+++ Actions/RegisterAction.py
@@ -1,41 +1,34 @@
 from Pages.RegisterPage import RegisterPage
+from Actions.BaseAction import BaseAction
 import time
 
-class RegisterAction:
+class RegisterAction(BaseAction):
+
     def __init__(self, driver):
-        self.driver = driver
+        super().__init__(driver)
         self.registerPage = RegisterPage()
 
     def click_register_link(self):
-
-        self.driver.find_element(self.registerPage.get_register_link()).click()
+        self.click(self.registerPage.get_register_link())
 
     def enter_first_name(self, first_name):
-
-        self.driver.find_element(self.registerPage.get_first_name()).send_keys(first_name)
+        self.send_keys(self.registerPage.get_first_name(), first_name)
 
     def enter_last_name(self, last_name):
-
-        self.driver.find_element(self.registerPage.get_last_name()).send_keys(last_name)
+        self.send_keys(self.registerPage.get_last_name(), last_name)
 
     def enter_email(self, email):
-
-        unique_email = (email +str(int(time.time())) +"@gmail.com")
-
-        self.driver.find_element(self.registerPage.get_email()).send_keys(unique_email)
+        unique_email = email + str(int(time.time())) + "@gmail.com"
+        self.send_keys(self.registerPage.get_email(), unique_email)
 
     def enter_password(self, password):
-
-        self.driver.find_element(self.registerPage.get_password()).send_keys(password)
+        self.send_keys(self.registerPage.get_password(), password)
 
     def enter_confirm_password(self, confirm_password):
-
-        self.driver.find_element(self.registerPage.get_confirm_password()).send_keys(confirm_password)
+        self.send_keys(self.registerPage.get_confirm_password(), confirm_password)
 
     def click_register_button(self):
-
-        self.driver.find_element(self.registerPage.get_register_button()).click()
+        self.click(self.registerPage.get_register_button())
 
     def get_success_message(self):
-
-        return self.driver.find_element(self.registerPage.get_success_message()).text
+        return self.get_text(self.registerPage.get_success_message())
