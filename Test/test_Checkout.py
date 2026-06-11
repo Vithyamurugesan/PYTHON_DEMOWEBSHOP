2fdb6f9050b34dcef80205b1364e908604be79f0
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-06-11 11:34:54 +0530

    completed 1 scenario

--- Test/test_Checkout.py
+++ Test/test_Checkout.py
@@ -19,16 +19,20 @@ class TestCheckout:
         actions.enter_password(ReadConfig.get_password())
         actions.click_login_button()
         assert actions.get_user_account_name() is not None
-        # Dummy function Stub needed cart Module 
+       
         cart = CartAction(self.driver)
         cart.open_books_page()
         cart.open_computing_book_page()
         cart.add_to_cart()
         cart.open_shopping_cart()
         cart.Click_checkout()
-        time.sleep(10)
+      
         # Cart()
 
         checkout=checkoutAction(self.driver)
         checkout.form_fill()
+        time.sleep(5)
+        act = checkout.shippingText()
+        exp="Select a shipping address from your address book or enter a new address."
+        assert act==exp,"assert is get error"
         time.sleep(10)
