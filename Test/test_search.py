e04762db4b7c4a9cc8d1ad1ef1922458bfdf2dbd
Vetri-Java-Dev <2k22cse161@kiot.ac.in>
2026-06-14 21:49:42 +0530

    Log added in address, search and wishlist tests

--- Test/test_search.py
+++ Test/test_search.py
@@ -1,38 +1,65 @@
 import pytest
+
 from Actions.SearchAction import SearchAction
 from Utilities import excelReader
+from Utilities.logCreator import get_logger
+
+
+logger = get_logger()
+
 
 @pytest.mark.Vetri
 @pytest.mark.usefixtures("setup_and_teardown")
 class TestSearch:
 
+
     @pytest.mark.parametrize("product", excelReader.get_data("TestData/TestData.xlsx","ValidSearchProducts"))
     def test_valid_search(self, product):
+
+        logger.info(f"Starting valid search test for product: {product}")
 
         search = SearchAction(self.driver)
 
         search.searchProduct(product)
+        logger.info(f"Entered search product: {product}")
+
         search.clickSearch()
+        logger.info("Clicked search button")
 
         assert search.verifySearchRedirect()
+        logger.info("Search redirect verified successfully")
+
 
 
     @pytest.mark.parametrize("product", excelReader.get_data("TestData/TestData.xlsx","InvalidSearchProducts"))
     def test_invalid_search(self, product):
 
+        logger.info(f"Starting invalid search test for product: {product}")
+
         search = SearchAction(self.driver)
 
         search.searchProduct(product)
+        logger.info(f"Entered invalid search product: {product}")
+
         search.clickSearch()
+        logger.info("Clicked search button")
 
         assert search.verify_no_product_message(),"No product message is not displayed"
-    
+        logger.info("No product message verified successfully")
+
+
 
     def test_search_without_keyword(self):
 
+        logger.info("Starting search without keyword test")
+
         search = SearchAction(self.driver)
+
         search.clickSearch()
+        logger.info("Clicked search button without entering keyword")
 
         assert search.is_alert_present(),"Warning message is not displayed"
+        logger.info("Warning alert message verified")
 
         search.accept_alert()
+        logger.info("Alert accepted successfully")
--- Utilities/logCreator.py
+++ Utilities/logCreator.py
@@ -1,19 +1,30 @@
 import logging
 import os
 
-def get_logger():
 
-    log_dir="./Logs"
+def get_logger(name="FrameworkLogger"):
+
+    log_dir = "./Logs"
 
     os.makedirs(log_dir, exist_ok=True)
 
-    logger = logging.getLogger("FrameworkLogger")
+    logger = logging.getLogger(name)
 
     if not logger.handlers:
+
         logger.setLevel(logging.INFO)
-        file_handler = logging.FileHandler("./Logs/log_report.log",mode="a")
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+        file_handler = logging.FileHandler(
+            os.path.join(log_dir, "log_report.log"),
+            mode="a"
+        )
+
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+        )
+
         file_handler.setFormatter(formatter)
+
         logger.addHandler(file_handler)
 
     return logger
