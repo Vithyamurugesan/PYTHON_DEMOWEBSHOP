e0432a4a91e26d24622e50cc5dac3899c37c7aa9
sowndariya <sowndariyadeveloper@gmail.com>
2026-07-14 20:43:06 +0530

    add static methods

--- Utilities/configReader.py
+++ Utilities/configReader.py
@@ -44,3 +44,39 @@ class ReadConfig:
     @staticmethod
     def get_empty_compare_msg():
         return config.get("compare info", "empty_compare_msg")
+
+    @staticmethod
+    def get_invalid_coupon_msg():
+        return config.get("cart info", "invalid_coupon_msg")
+
+    @staticmethod
+    def get_empty_cart_msg():
+        return config.get("cart info", "empty_cart_msg")
+
+    @staticmethod
+    def get_updated_quantity():
+        return config.get("cart info", "updated_quantity")
+
+    @staticmethod 
+    def get_availability_label():
+        return config.get("product detail info", "availability_label")
+
+    @staticmethod
+    def get_invalid_qty_msg():
+        return config.get("product detail info", "invalid_qty_msg")
+
+    @staticmethod
+    def get_first_name():
+        return config.get("empty_fields_test","first_name")
+
+    @staticmethod
+    def get_last_name():
+        return config.get("empty_fields_test","last_name")
+
+    @staticmethod
+    def get_email_register():
+        return config.get("empty_fields_test","email")
+
+    @staticmethod  
+    def get_password_required_error():
+        return config.get("empty_fields_test","password_required")
