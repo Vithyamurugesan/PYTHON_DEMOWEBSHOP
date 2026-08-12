d509f73567e36975765cbc9c2cec05304510ff1e
sowndariya <sowndariyadeveloper@gmail.com>
2026-08-12 14:54:30 +0530

    update product detail testdata

--- Utilities/CsvReader.py
+++ Utilities/CsvReader.py
@@ -13,24 +13,17 @@ class CsvReader:
 
     @staticmethod
     def get_data(file_path):
-
         data = []
-
         with open(
-            CsvReader._resolve_path(file_path),
-            mode="r",
-            newline="",
-            encoding="utf-8"
-        ) as file:
-
-            reader = csv.reader(file)
-
-            next(reader)  # Skip header
-
+        CsvReader._resolve_path(file_path),
+        mode="r",
+        newline="",
+        encoding="utf-8"
+    ) as file:
+            reader = csv.DictReader(file)
             for row in reader:
                 data.append(row)
-
-        return data
+                return data
 
     @staticmethod
     def get_wishlist_data(file_path, testcase):
