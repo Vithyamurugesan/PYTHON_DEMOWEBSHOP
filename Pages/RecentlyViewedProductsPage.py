9ac66800bc3d9e721a2bf0b4021eafa5c328d5fc
sowndariya <sowndariyadeveloper@gmail.com>
2026-08-15 21:16:53 +0530

    new feature recently added product

---
+++ Pages/RecentlyViewedProductsPage.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.common.by import By
+
+
+class RecentlyViewedProductsPage:
+
+    def __init__(self):
+        self.__category_link = lambda cat: (By.LINK_TEXT, cat)
+        self.__product_link = lambda name: (By.LINK_TEXT, name)
+        self.__recently_viewed_products_link = (By.LINK_TEXT, "Recently viewed products")
+        self.__page_title = (By.XPATH, "//div[@class='page-title']/h1")
+        self.__product_items = (By.XPATH, "//div[contains(@class,'product-item')]")
+        self.__product_titles = (By.XPATH, "//div[contains(@class,'product-item')]//h2[@class='product-title']/a")
+
+    def get_category_link(self, category):
+        return self.__category_link(category)
+
+    def get_product_link(self, name):
+        return self.__product_link(name)
+
+    def get_recently_viewed_products_link(self):
+        return self.__recently_viewed_products_link
+
+    def get_page_title(self):
+        return self.__page_title
+
+    def get_product_items(self):
+        return self.__product_items
+
+    def get_product_titles(self):
+        return self.__product_titles
---
+++ Test/test_recentlyviewedproducts.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium.webdriver.remote.webdriver import WebDriver
+from Actions.RecentlyViewedProductsAction import RecentlyViewedProductsAction
+
+RECENTLY_VIEWED_CSV = "TestData/RecentlyViewedProductsData.csv"
+
+
+@pytest.mark.usefixtures("setup_and_teardown")
+class TestRecentlyViewedProducts:
+    driver: WebDriver
+
+    def test_recently_viewed_products_page_opens(self):
+        recently_viewed = RecentlyViewedProductsAction(self.driver)
+        recently_viewed.open_recently_viewed_products_page()
+        assert recently_viewed.is_recently_viewed_products_page_opened(), \
+            "Recently Viewed Products page did not open"
+
+    def test_viewed_product_is_displayed(self):
+        recently_viewed = RecentlyViewedProductsAction(self.driver)
+        recently_viewed.open_first_product_from_csv(RECENTLY_VIEWED_CSV)
+        recently_viewed.open_recently_viewed_products_page()
+        assert recently_viewed.is_first_visited_product_displayed(), \
+            "Viewed product is not displayed in Recently Viewed Products"
+
+    def test_multiple_viewed_products_are_displayed(self):
+        recently_viewed = RecentlyViewedProductsAction(self.driver)
+        recently_viewed.open_all_products_from_csv(RECENTLY_VIEWED_CSV)
+        recently_viewed.open_recently_viewed_products_page()
+        assert recently_viewed.are_all_visited_products_displayed(), \
+            "All viewed products are not displayed in Recently Viewed Products"
